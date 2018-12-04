20561cfba98d58385011093007e54eb47d329409
Nicholas Zachary <nicholas.zachary@outlook.com>
2018-12-04 01:59:13 +0000

    answer

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var secret  = require('secret');
+var secret  = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -8,14 +8,23 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'authorization':
+      'Authorization':secret.GITHUB_TOKEN
     }
   };
 
+
   request(options, function(err, res, body) {
-    cb(err, body);
+    let resObj = JSON.parse(body);
+    //let stringfyObj = JSON.stringify(resObj, null, 2)
+    cb(err, resObj);
   });
 
 }
 
-curl -i -H 'Authorization: token 89d61a0643a27c044a59fd1de81a23f166fc5b80' https://api.github.com/repos/jquery/jquery/contributors
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result[0].avatar_url);
+
+});
+// curl -i -H 'Authorization: token 89d61a0643a27c044a59fd1de81a23f166fc5b80' https://api.github.com/repos/jquery/jquery/contributors
